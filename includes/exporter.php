822507a8f7d70a2beb52ee032f4966339a983ae0
jeremyBass <jeremybass@cableone.net>
2014-07-01 13:25:25 -0700

    refine the test functions

--- includes/exporter.php
+++ includes/exporter.php
@@ -140,30 +140,38 @@ $skip=array(
 	$exportData .= $settings['outputOpenData'];
 }
 function isJson($string) {
- json_decode($string);
- return (json_last_error() == JSON_ERROR_NONE);
+    if ( !is_string( $string ) ){
+        return false;
+	}
+	json_decode($string);
+	return (json_last_error() == JSON_ERROR_NONE);
 }
 function is_serialized( $data ) {
     // if it isn't a string, it isn't serialized
-    if ( !is_string( $data ) )
+    if ( !is_string( $data ) ){
         return false;
+	}
     $data = trim( $data );
-    if ( 'N;' == $data )
+    if ( 'N;' == $data ){
         return true;
-    if ( !preg_match( '/^([adObis]):/', $data, $badions ) )
+	}
+    if ( !preg_match( '/^([adObis]):/', $data, $badions ) ){
         return false;
+	}
     switch ( $badions[1] ) {
         case 'a' :
         case 'O' :
         case 's' :
-            if ( preg_match( "/^{$badions[1]}:[0-9]+:.*[;}]\$/s", $data ) )
+            if ( preg_match( "/^{$badions[1]}:[0-9]+:.*[;}]\$/s", $data ) ){
                 return true;
+			}
             break;
         case 'b' :
         case 'i' :
         case 'd' :
-            if ( preg_match( "/^{$badions[1]}:[0-9.E-]+;\$/", $data ) )
+            if ( preg_match( "/^{$badions[1]}:[0-9.E-]+;\$/", $data ) ){
                 return true;
+			}
             break;
     }
     return false;
