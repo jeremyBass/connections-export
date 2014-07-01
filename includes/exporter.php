a05be75c59b6eafee6d20a5855d34e4b6eb887e5
jeremyBass <jeremybass@cableone.net>
2014-07-01 13:06:46 -0700

    gets the csv exporting pretty nice it seem with out errors. so add a button

--- includes/exporter.php
+++ includes/exporter.php
@@ -6,6 +6,10 @@ $file	=	'export';
 // You can change this to a non-live settings table...
 $settingsTable = "wp_connections_export_settings";
 
+
+
+
+
 // Open the database, get the export settings, and set up the export fields...
 openExport();
 
@@ -37,7 +41,23 @@ function openExport() {
 			define( 'CN_TERM_RELATIONSHIP_TABLE', $prefix . 'connections_term_relationships' );
 			*/	
 	
-	global	$wpdb, $exportData, $contacts, $exportFields, $settings, $settingsTable, $numContacts;
+	global	$wpdb, $exportData, $contacts, $exportFields, $settings, $settingsTable, $numContacts,$skip;
+
+
+$skip=array(
+	"options",
+	"slug",
+	"ts",
+	"family_name",
+	"honorific_prefix",
+	"first_name",
+	"middle_name",
+	"last_name",
+	"honorific_suffix",
+	"title"
+);
+
+
 
 	$contacts = $wpdb->get_results( "SELECT * FROM ".CN_ENTRY_TABLE, ARRAY_A );
 	$numContacts = count($contacts);
@@ -79,6 +99,8 @@ function openExport() {
 			$settings['outputCloseDelim']	= "</td>\r\n";
 			$settings['outputCloseRec']		= "\t</tr>\r\n";
 			$settings['outputCloseData']	= "</table>\r\n";
+			$settings['dbl_escape']				= '';
+			$settings['dbl_escapeWith']			= '';
 			$settings['escape']				= '&';
 			$settings['escapeWith']			= '&amp;';
 			break;
@@ -91,6 +113,8 @@ function openExport() {
 			$settings['outputCloseDelim']	= "\t";
 			$settings['outputCloseRec']		= "\r\n";
 			$settings['outputCloseData']	= "";
+			$settings['dbl_escape']				= '';
+			$settings['dbl_escapeWith']			= '';
 			$settings['escape']				= "\t";
 			$settings['escapeWith']			= "\t\t";
 			break;
@@ -103,8 +127,11 @@ function openExport() {
 			$settings['outputCloseDelim']	= '",';
 			$settings['outputCloseRec']		= "\r\n";
 			$settings['outputCloseData']	= "";
-			$settings['escape']				= '"';
-			$settings['escapeWith']			= '""';
+			
+			$settings['dbl_escape']				= '"';
+			$settings['dbl_escapeWith']			= '""';
+			$settings['escape']				= "'";
+			$settings['escapeWith']			= "''";
 			break;
 	}
 
@@ -116,16 +143,23 @@ function isJson($string) {
  json_decode($string);
  return (json_last_error() == JSON_ERROR_NONE);
 }
+
 // Writes out the data fields...
-function exportCells() {
-	global $settings,$contacts, $exportData, $exportFields, $maxCategories, $wpdb;
-	
-	
+function create_header() {
+	global $settings,$contacts, $exportData, $exportFields, $maxCategories, $wpdb, $header,$skip;
+
 	$dataset = '';
+	
+	
+	$head=array();
 	// Go through each contact...
 	foreach($contacts as $contact) {
 		
-		
+		foreach($contact as $key=>$part){
+			if(!isset($head[$key]) && !in_array($key,$skip)){
+				$head[$key]=$key;
+			}
+		}		
 		$meta = $wpdb->get_results( "SELECT * FROM ".CN_ENTRY_TABLE_META." WHERE `entry_id`=".$contact['id'], ARRAY_A );
 		
 		
@@ -136,29 +170,88 @@ function exportCells() {
 				$json_value = json_decode($value);
 				foreach($json_value as $jkey=>$jpart){
 					$true_value=($jpart=='0')?'false':($jpart=='1')?'true':$jpart;
-					$contact[$key."::".$jkey]=$true_value;	
+					$contact[]=$true_value;	
+					
+					if(!isset($head[$key."::".$jkey]) && !in_array($key."::".$jkey,$skip)){
+						$head[$key."::".$jkey]=$key."::".$jkey;
+					}
+					
+					
 				}
 			}else{
-				$contact[$key]=$value;	
+				if(!isset($head[$key]) && !in_array($key,$skip)){
+					$head[$key]=$key;
+				}
 			}
 		}
-		$rec = '';
-		foreach($contact as $key=>$part){
-			$value=part_filter(c,$part);
-			$rec.=$settings['outputOpenDelim'].$part.$settings['outputCloseDelim'];
+		
+
+		
+	}
+	
+	$rec = '';
+	$header=$head;
+	foreach($head as $part){
+		$rec.=$settings['outputOpenDelim'].$part.$settings['outputCloseDelim'];
+	}
+	$dataset .= exportRecord($rec);
+	
+	
+	// Now write the data...
+	$exportData .= $dataset;
+}
+
+
+
+// Writes out the data fields...
+function exportCells() {
+	global $settings,$contacts, $exportData, $exportFields, $maxCategories, $wpdb, $header,$skip;
+	create_header();
+	
+	$dataset = '';
+	// Go through each contact...
+	foreach($contacts as $contact) {
+		
+		
+		$meta = $wpdb->get_results( "SELECT * FROM ".CN_ENTRY_TABLE_META." WHERE `entry_id`=".$contact['id'], ARRAY_A );
+
+		foreach($meta as $part){
+			$key=$part['meta_key'];
+			$value=$part['meta_value'];
+			if(isJson($value)){
+				$json_value = json_decode($value);
+				foreach($json_value as $jkey=>$jpart){
+					$true_value=($jpart=='0')?'false':($jpart=='1')?'true':$jpart;
+					$contact[$key."::".$jkey]=$true_value;
+				}
+			}else{
+				$contact[$key]=$value;
+			}
+		}
+
+		
+		foreach($header as $col){//keeps the col and head lined up
+			$rec = '';
+			foreach($contact as $key=>$part){
+				if($col=$key){
+					if(!in_array($key,$skip)){
+						$value=part_filter($key,$part);
+						$rec.=data($value);
+					}
+				}
+			}
 		}
 		$dataset .= exportRecord($rec);
 	}
 	// Now write the data...
 	$exportData .= $dataset;
-	
 }
 
 function part_filter($key,$part){
 	
 	switch (strtolower($key)) {
 		case "date_added":
-			$part=date('l jS \of F Y h:i:s A',$part);
+			$part=date('Y-m-d H:i:s',$part);
 			break;
 	}
 	
@@ -175,127 +268,6 @@ function exportHeader() {
 	$exportData .= $settings['outputOpenHeader'] . $header . $settings['outputCloseHeader'];
 }
 
-// This is called for each breakout field encountered while writing the header, it returns all header cells that needed to be drawn by the breakout.
-// It also populates the fields and types array strings if they're empty.
-function explodeBreakoutHeader(&$breakout) {
-	global $db, $maxCategories,$wpdb;
-
-	// We need a list of fields (i.e. adr_line1, adr_line2, city, state, zip), and a list of types (i.e. work, home, other)
-
-	// If 'table_name' doesn't exist, put the contents of 'field' into it (this step allows for odd things like the dates field/date table)...
-	if (empty($breakout['table_name'])) $breakout['table_name'] = $breakout['field'];
-
-	// Get an array of each field we need to use...
-	$breakoutFields = explode(";", $breakout['fields']);
-
-	// If no breakout field list was specified, include all fields...
-	if (empty($breakout['fields'])) {
-		// Get the field names from the SQL schema for the table we're going to use, and plop them into an array...
-		$result = $wpdb->get_col("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = '".$db."' AND table_name = 'wp_connections_".$breakout['table_name']."';");
-		$i=0;
-		foreach ($result as $value) {
-			$breakoutFields[$i] = $value[0];
-		}
-		// Copy the array back into the fields item for use later...
-		$breakout['fields'] = implode(";", $breakoutFields);
-	}
-
-	// ### Take care of the types...
-
-	// Get an array of each type we need to use...
-	$breakoutTypes = explode(";", $breakout['breakout_types']);
-	// You can specify you only want home addresses in an export for example, if nothing is specified, get a list of all types from the breakout's table...
-	if (empty($breakout['breakout_types'])) {
-		// Put the result into an array...
-		$result = $wpdb->get_col("SELECT DISTINCT type FROM wp_connections_".$breakout['table_name']." ORDER BY type");
-		// Put a list of types for this breakout into an array...
-		$i=0;
-		foreach ($result as $value) {
-			$breakoutTypes[$i] = $value['type'];
-		}
-		// Copy the array back into the breakout_types item for use later...
-		$breakout['breakout_types'] = implode(";", $breakoutTypes);
-	}
-
-	// ### When we get here, both the fields and types are checked and ready to go in 2 arrays, we just need to write the breakout...
-
-	// Handle different types...
-	switch ($breakout['type']) {
-		// Explode all field columns and types...
-		case 1:
-			foreach ($breakoutTypes as $type) {
-				foreach ($breakoutFields as $field) {
-					$line .= exportBreakoutHeaderField($breakout, $field, $type);
-				}
-			}
-			break;
-		// Joined from another table
-		case 2:
-			$line .= data($breakout['display_as']);
-			break;
-		// Breakout a list in the header...
-		case 3:
-			$rTemp = $wpdb->get_col("SELECT id FROM wp_connections");
-			// Go through each contact...
-			$maxCategories = 0;
-			foreach($rTemp as $value){
-				// And get a count of how many categories it has...
-				$rTemp2 = $wpdb->get_var("SELECT count(*) FROM wp_connections_terms JOIN wp_connections_term_relationships ON wp_connections_term_relationships.term_taxonomy_id = wp_connections_terms.term_id WHERE wp_connections_term_relationships.entry_id = ".$value['id']);
-				// Find the biggest result...
-				if ($rTemp2 > $maxCategories) $maxCategories = $rTemp2;
-			}
-
-			// Finally, write a list of fields for each category...
-			for ($i = 1; $i < $maxCategories+1; $i++) {
-				$line .= data('Category '.$i);
-			}
-			break;
-		// Breakout a list in the header, using primaries in the first column...
-		case 4:
-			$rTemp = $wpdb->get_col("SELECT id FROM wp_connections");
-			// Go through each contact...
-			$maxCategories = 0;
-			foreach($rTemp as $value){
-				// And get a count of how many categories it has...
-				$rTemp2 = $wpdb->get_var("SELECT count(*) FROM wp_connections_terms JOIN wp_connections_term_relationships ON wp_connections_term_relationships.term_taxonomy_id = wp_connections_terms.term_id WHERE wp_connections_term_relationships.entry_id = ".$value['id']);
-				$res = mysql_fetch_array($rTemp2);
-				// Find the biggest result...
-				if ($rTemp2 > $maxCategories) $maxCategories = $rTemp2;
-			}
-
-			// Finally, write a list of fields for each category...
-			for ($i = 0; $i < $maxCategories+1; $i++) {
-				if ($i == 0) {
-					$line .= data('Main Category');
-				} else {
-					$line .= data('Sub-Cat '.$i);
-				}
-			}
-			break;
-	}
-
-	return $line;
-}
-
-// outputs a breakout header type...
-function exportBreakoutHeaderField($breakout, $field, $type) {
-	global $settings;
-	if ($settings['ucfirst'] == 1) {
-		$field = ucfirst($field);
-		$type = ucfirst($type);
-	}
-	// Display the field name based on settings...
-	switch (strtolower($breakout['display'])) {
-		case 'pre':
-			return data($type .' '. $field);
-		case 'post':
-			return data($field .' '. $type);
-		case 'only':
-			return data($type);
-		default:
-			return data($field);
-	}
-}
 
 // Writes out an entire record to the $exportData string...
 function exportRecord($data) {
@@ -307,6 +279,7 @@ function exportRecord($data) {
 function data($data) {
 	global $settings;
 	$data = str_replace($settings['escape'], $settings['escapeWith'], $data);
+	$data = str_replace($settings['dbl_escape'], $settings['dbl_escapeWith'], $data);
 	$data = str_replace('&amp;', '&', $data);
 	$data = str_replace('&nbsp;', ' ', $data);
 	return $settings['outputOpenDelim'] . $data . $settings['outputCloseDelim'];
@@ -315,6 +288,11 @@ function data($data) {
 function closeExport() {
 	global $settings, $exportData, $file;
 	$file = 'export';
+	
+	//var_dump($exportData);die();
+	
+	
+	
 	// Write the close statement to the export data stream...
 	$exportData .= $settings['outputCloseData'];
 	// Process special instructions for closing the export...
@@ -336,5 +314,6 @@ function closeExport() {
 			header( "Content-disposition: filename=".$filename.".csv");
 			print $exportData;
 	}
+	exit();
 }
 ?>
