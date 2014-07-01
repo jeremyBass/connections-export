91c2fddb1b4d4c9aa83411e76da0b3f99b1718c8
jeremyBass <jeremybass@cableone.net>
2014-07-01 13:27:43 -0700

    wordpress has is_serialized()

--- includes/exporter.php
+++ includes/exporter.php
@@ -146,36 +146,7 @@ function isJson($string) {
 	json_decode($string);
 	return (json_last_error() == JSON_ERROR_NONE);
 }
-function is_serialized( $data ) {
-    // if it isn't a string, it isn't serialized
-    if ( !is_string( $data ) ){
-        return false;
-	}
-    $data = trim( $data );
-    if ( 'N;' == $data ){
-        return true;
-	}
-    if ( !preg_match( '/^([adObis]):/', $data, $badions ) ){
-        return false;
-	}
-    switch ( $badions[1] ) {
-        case 'a' :
-        case 'O' :
-        case 's' :
-            if ( preg_match( "/^{$badions[1]}:[0-9]+:.*[;}]\$/s", $data ) ){
-                return true;
-			}
-            break;
-        case 'b' :
-        case 'i' :
-        case 'd' :
-            if ( preg_match( "/^{$badions[1]}:[0-9.E-]+;\$/", $data ) ){
-                return true;
-			}
-            break;
-    }
-    return false;
-}
+
 // Writes out the data fields...
 function create_header() {
 	global $settings,$contacts, $exportData, $exportFields, $maxCategories, $wpdb, $header,$skip;
@@ -268,6 +239,14 @@ function exportCells() {
 				if($col=$key){
 					if(!in_array($key,$skip)){
 						$value=part_filter($key,$part);
+						
+						if(is_serialized($value )){
+							$value=$value;
+							var_dump($value);die();
+						}
+						
+						
+						
 						$rec.=data($value);
 					}
 				}
